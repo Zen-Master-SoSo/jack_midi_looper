b3a6385291b67f13680b0e2db6c210f5b9cfc4bd
Liyang <liyang@holly.home>
2024-11-25 11:39:55 +0800

    Make Loops instance -type, instead of all class methods

--- loops.py
+++ loops.py
@@ -17,10 +17,9 @@ USECS_PER_SECOND = 1000000
 
 class Loop:
 
-	def __init__(self, loop_id):
-		cursor = Loops.conn().cursor()
-		cursor.execute('SELECT * FROM loops WHERE loop_id = ?', (loop_id,))
-		self.loop_id, self.loop_group, self.name, self.beats_per_measure, self.measures, midi_events = cursor.fetchone()
+	def __init__(self, fetched_row):
+		self.loop_id, self.loop_group, self.name, \
+			self.beats_per_measure, self.measures, midi_events = fetched_row
 		evfile = io.BytesIO(midi_events)
 		self.events = np.load(evfile)
 		self._beat_offset = 0
@@ -60,7 +59,7 @@ class Loop:
 		return self.events[(self.events['beat'] >= start) & (self.events['beat'] < end)]
 
 	def __str__(self):
-		return '<Loop #{0.loop_id}: "{0.name}"; {0.beats_per_measure} beats per measure; {0.measures} measures; {0.event_count} events>'.format(self)
+		return '<Loop #{0.loop_id}: "{0.name}", {0.beats_per_measure} beats per measure, {0.measures} measures, {0.event_count} events>'.format(self)
 
 	def print_events(self):
 		for i in range(len(self.events)):
@@ -74,34 +73,25 @@ class Loops:
 	_loop_ids = None
 	_groups = None
 
-	@classmethod
-	def dbfile(cls):
-		try:
-			os.mkdir(os.path.join(user_config_dir(), 'ZenSoSo'))
-		except FileExistsError:
-			pass
-		return os.path.join(user_config_dir(), 'ZenSoSo', 'midibanks.db')
+	def __init__(self, dbfile):
+		self.dbfile = dbfile
+		self._connection = sqlite3.connect(self.dbfile)
+		self._connection.execute('PRAGMA foreign_keys = ON')
+		cursor = self._connection.execute('SELECT name FROM sqlite_master WHERE type="table"')
+		rows = cursor.fetchall()
+		if len(rows) == 0:
+			self.init_schema()
 
-	@classmethod
-	def conn(cls):
-		if cls._connection is None:
-			cls._connection = sqlite3.connect(cls.dbfile())
-			cls._connection.execute('PRAGMA foreign_keys = ON')
-			cls._connection.execute('PRAGMA foreign_keys = ON')
-			cursor = cls._connection.execute('SELECT name FROM sqlite_master WHERE type="table"')
-			rows = cursor.fetchall()
-			if len(rows) == 0:
-				cls.init_schema()
-		return cls._connection
+	def conn(self):
+		return self._connection
 
-	@classmethod
-	def init_schema(cls):
-		cls.conn().execute("DROP INDEX IF EXISTS bpm_index")
-		cls.conn().execute("DROP INDEX IF EXISTS measures_index")
-		cls.conn().execute("DROP INDEX IF EXISTS pitch_index")
-		cls.conn().execute("DROP TABLE IF EXISTS pitches")
-		cls.conn().execute("DROP TABLE IF EXISTS loops")
-		cls.conn().execute("""
+	def init_schema(self):
+		self._connection.execute("DROP INDEX IF EXISTS bpm_index")
+		self._connection.execute("DROP INDEX IF EXISTS measures_index")
+		self._connection.execute("DROP INDEX IF EXISTS pitch_index")
+		self._connection.execute("DROP TABLE IF EXISTS pitches")
+		self._connection.execute("DROP TABLE IF EXISTS loops")
+		self._connection.execute("""
 			CREATE TABLE loops (
 				loop_id INTEGER PRIMARY KEY,
 				loop_group TEXT,
@@ -110,24 +100,22 @@ class Loops:
 				measures INTEGER,
 				midi_events BLOB
 			)""")
-		cls.conn().execute("""
+		self._connection.execute("""
 			CREATE TABLE pitches (
 				loop_id INTEGER,
 				pitch INTEGER,
 				FOREIGN KEY(loop_id) REFERENCES loops(loop_id) ON DELETE CASCADE
 			)""")
-		cls.conn().execute("CREATE INDEX bpm_index ON loops (beats_per_measure)")
-		cls.conn().execute("CREATE INDEX measures_index ON loops (measures)")
-		cls.conn().execute("CREATE INDEX pitch_index ON pitches (pitch)")
+		self._connection.execute("CREATE INDEX bpm_index ON loops (beats_per_measure)")
+		self._connection.execute("CREATE INDEX measures_index ON loops (measures)")
+		self._connection.execute("CREATE INDEX pitch_index ON pitches (pitch)")
 
-	@classmethod
-	def delete_all(cls):
-		cls.conn().execute("DELETE FROM loops")
-		cls.conn().commit()
+	def delete_all(self):
+		self._connection.execute("DELETE FROM loops")
+		self._connection.commit()
 
-	@classmethod
-	def import_dirs(cls, base_dir):
-		cursor = cls.conn().cursor()
+	def import_dirs(self, base_dir):
+		cursor = self._connection.cursor()
 		loop_sql = """
 			INSERT INTO loops(loop_group, name, beats_per_measure, measures, midi_events)
 			VALUES (?,?,?,?,?)
@@ -140,13 +128,13 @@ class Loops:
 			loop_group = re.sub(r'(_|[^\w])+', ' ', os.path.dirname(filename).replace(base_dir, ''))
 			name = os.path.splitext(os.path.basename(filename))[0]
 			try:
-				beats_per_measure, measures, pitches, events = cls.read_midi_file(filename)
+				beats_per_measure, measures, pitches, events = self.read_midi_file(filename)
 				evfile = io.BytesIO()
 				np.save(evfile, events)
 				evfile.seek(0)
 				cursor.execute(loop_sql, (loop_group, name, beats_per_measure, measures, evfile.read()))
 				cursor.executemany(pitch_sql, [ (cursor.lastrowid, pitch) for pitch in pitches ])
-				cls.conn().commit()
+				self._connection.commit()
 			except Exception as e:
 				print('Failed to import {}. ERROR {} "{}".'.format(name, type(e).__name__, e))
 
@@ -159,7 +147,6 @@ class Loops:
 			pitches				: (set) pitches of a noteon events
 			events				: nparray of EVENT_STRUCT
 		"""
-
 		# Use mido to open
 		mid = MidiFile(midi_filename)
 		# Default calculations, overriden by set_tempo and time_signature events
@@ -178,7 +165,6 @@ class Loops:
 		ordinal = 0
 		measure = 0
 		pitches = []
-
 		for msg in mid:
 			if msg.type == 'set_tempo':
 				usecs_per_beat = msg.tempo
@@ -194,41 +180,45 @@ class Loops:
 				ordinal += 1
 				pitches.append(msg.note)
 			time += msg.time
-
 		return int(beats_per_measure), measure + 1, set(pitches), events
 
-	@classmethod
-	def groups(cls):
-		if cls._groups is None:
-			cursor = Loops.conn().cursor()
+	def groups(self):
+		if self._groups is None:
+			cursor = self._connection.cursor()
 			cursor.execute('SELECT DISTINCT(loop_group) FROM loops')
-			cls._groups = [ row[0] for row in cursor.fetchall() ]
-		return cls._groups
+			self._groups = [ row[0] for row in cursor.fetchall() ]
+		return self._groups
 
-	@classmethod
-	def group_loops(cls, loop_group):
+	def group_loops(self, loop_group):
 		"""
 		Returns list of tuples, each containing (loop_id, name)
 		"""
-		cursor = Loops.conn().cursor()
+		cursor = self._connection.cursor()
 		cursor.execute('SELECT loop_id, name FROM loops WHERE loop_group = ?', (loop_group,))
 		return cursor.fetchall()
 
-	@classmethod
-	def loop_ids(cls):
-		if cls._loop_ids is None:
-			cursor = Loops.conn().cursor()
+	def loop_ids(self):
+		if self._loop_ids is None:
+			cursor = self._connection.cursor()
 			cursor.execute('SELECT loop_id FROM loops')
-			cls._loop_ids = [ row[0] for row in cursor.fetchall() ]
-		return cls._loop_ids
+			self._loop_ids = [ row[0] for row in cursor.fetchall() ]
+		return self._loop_ids
 
-	@classmethod
-	def random_loop(cls):
-		return Loop(choice(cls.loop_ids()))
+	def loop(self, loop_id):
+		cursor = self._connection.cursor()
+		cursor.execute('SELECT * FROM loops WHERE loop_id = ?', (loop_id,))
+		return Loop(cursor.fetchone())
+
+	def random_loop(self):
+		return self.loop(choice(self.loop_ids()))
 
 
 
 if __name__ == "__main__":
-	print(len(Loops.loop_ids()), 'loops')
+	loops = Loops(os.path.join(user_config_dir(), 'ZenSoSo', 'midibanks.db'))
+	print(len(loops.groups()), 'groups')
+	print(len(loops.loop_ids()), 'loops')
+	print('Random loop:')
+	print(loops.random_loop())
 
 #  end jack_midi_looper/utils/loops.py
